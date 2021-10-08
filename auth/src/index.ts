47a646b0f032cfdfd54a8b7ad879b60078a440cf
Kyle Glover <kjglover4585@gmail.com>
2021-10-08 02:58:03 -0700

    test for the deployment of auth to digital ocean hosting

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
-  console.log("Starting Up.....");
+  console.log("Starting Up......");
 
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
