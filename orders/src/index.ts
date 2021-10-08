a818f7f9c261c53f20f7954afdd437803e27b1f3
Kyle Glover <kjglover4585@gmail.com>
2021-10-08 01:33:42 -0700

    Test github actions

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -9,6 +9,7 @@ import { PaymentCreatedListener } from "./events/listener/payment-created-listen
 import { PlantDeletedListener } from "./events/listener/plant-deleted-listener";
 
 const start = async () => {
+  console.log("Starting....");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
@@ -44,11 +45,7 @@ const start = async () => {
     new PaymentCreatedListener(natsWrapper.client).listen();
     new PlantDeletedListener(natsWrapper.client).listen();
 
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDb");
   } catch (err) {
     console.error(err);
