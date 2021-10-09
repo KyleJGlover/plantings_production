542455050a169f7c427006a15bd43f40af0d16d9
Kyle Glover <kjglover4585@gmail.com>
2021-10-09 16:31:55 -0700

    Attempting to initailize a new cluster

--- plants/src/index.ts
+++ plants/src/index.ts
@@ -6,7 +6,7 @@ import { OrderCreatedListener } from "./events/listener/order-created-listener";
 import { OrderCancelledListener } from "./events/listener/order-cancelled-listener";
 
 const start = async () => {
-  console.log("Starting...");
+  console.log("Starting....");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
